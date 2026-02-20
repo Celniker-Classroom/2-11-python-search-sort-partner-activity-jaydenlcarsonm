9b439a1230be599c7c2c1ab7391196bf64819353
BrokenBentoBox <147872492+BrokenBentoBox@users.noreply.github.com>
2026-02-20 17:01:38 +0000

    part2. done

--- part3.py
+++ part3.py
@@ -4,14 +4,12 @@ from random import randint #This line imports the randint function from the rand
 
 ranNums = [] #name your list and make sure it is empty!
 
-randomNum = []
+randomNum = input("Enter number of choice:")
+
 
 # Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
 for index in range(10): #for loop appends 5 numbers to your list, but make sure you name your variable
     ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
-
-for index in range(1):
-    randomNum.append(randint(1,50))
 
 print("Generated List:", ranNums) #print the list!
 
