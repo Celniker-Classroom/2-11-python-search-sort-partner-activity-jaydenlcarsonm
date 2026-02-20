d7b76e418e75c0448172bfb8278d5747ced6180d
BrokenBentoBox <147872492+BrokenBentoBox@users.noreply.github.com>
2026-02-20 17:38:26 +0000

    part3 done

--- part3.py
+++ part3.py
@@ -4,16 +4,16 @@ from random import randint #This line imports the randint function from the rand
 
 ranNums = [] #name your list and make sure it is empty!
 
-randomNum = input("Enter number of choice:")
+randomNum = input("Enter number of choice: ")
 
 
 # Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
 for index in range(10): #for loop appends 5 numbers to your list, but make sure you name your variable
     ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
 
-print("Generated List:", ranNums) #print the list!
+print("Generated List: ", ranNums) #print the list!
 
-print("Searching for number:", randomNum)
+print("Searching for number: ", randomNum)
 
 comparisons = 0  # Initialize the counter for comparisons
 found = False  # Variable to track if the number was found
@@ -28,3 +28,32 @@ if randomNum[0] in ranNums:
     print("Number",randomNum,"is found in the list after", comparisons, "comparison/s!")
 else:
     print("Number",randomNum,"not found in the list after", comparisons, "comparison/s!")
+
+ranNums.sort()
+print("Sorted List: ", ranNums)
+
+def calsum(ranNums):
+    total=0
+    for number in ranNums:
+        total += number
+    return total
+result = calsum(ranNums)
+
+print("The sum of the values is ", result) #sum of the values
+
+max4=max(ranNums)
+min4=min(ranNums)
+
+print("The biggest number is: ", max4) #max number
+print("The smallest number is: ", min4) #min number
+
+binarylist = [bin(x)[2:] for x in ranNums] #numbers into binary
+print(binarylist)
+
+Relist = [] #Reverse Order list
+for index in range(len(ranNums) -1, -1, -1):
+    Relist.append(ranNums[index])
+print("Numbers in reverse order: ", Relist)
+
+
+
